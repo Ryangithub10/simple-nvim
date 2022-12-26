35aba432e3b02e5dbf48b344e396c4abc1fc1054
Muhammad Febrian <technogamers543@gmail.com>
2022-12-27 02:06:42 +0800

    just little refactoring to lualine and nvim-tree

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -1,6 +1,10 @@
-require('lualine').setup({
+local status_ok, lualine = pcall(require, 'lualine')
+if not status_ok then
+    return
+end
+
+lualine.setup({
     sections = {
-        lualine_b = {'branch'},
         lualine_x = {'filetype'},
     },
 })
